4a907711218b08904686e6781fcd599411c29fb3
GABRIEL <gabriel-moreira98@hotmail.com>
2020-03-17 15:17:54 -0400

    hm

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
